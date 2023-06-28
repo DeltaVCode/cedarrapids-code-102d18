61c4dabe1c20c781ede1946003ca2be4d98f2205
tektechnologies <info@tektechnologies.com>
2023-06-28 11:21:00 -0500

    add demo

---
+++ class-05/demo/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("Hello World, how are you doing?");
+
+console.log(5 + 5 + 5);
+
+let five = 5;
+let ten = 10;
+//                       + connects text with the math.
+console.log(five + ten + " = 15");
